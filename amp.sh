e0c01b69d96593defb05222543093dfdcb820ba7
nf <nf@wh3rd.net>
2026-01-15 16:00:51 +1100

    support for go

--- amp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-ROOT="$(realpath "$(dirname "$0")")"
-POLICY="$(make-sandbox-policy "$ROOT")"
-
-sandbox-exec -p "$POLICY" amp
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"os/user"
 	"path/filepath"
 	"slices"
@@ -26,33 +28,80 @@ var allowRead = []string{
 	".gitconfig",
 }
 
-func parentPaths(base string, in ...string) (out []string) {
-	for _, p := range in {
-		p = filepath.Dir(p)
-		for p != "." {
-			out = append(out, filepath.Join(base, p))
-			p = filepath.Dir(p)
-		}
+func main() {
+	log.SetPrefix("make-sandbox-policy: ")
+
+	flag.Parse()
+	root := flag.Arg(0)
+	if root == "" {
+		fmt.Fprintf(os.Stderr, "usage: make-sandbox-policy <root>\n")
+		os.Exit(2)
 	}
-	return
+
+	profile, err := makeProfile(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(profile)
 }
 
-func makeProfile(home, root string) (string, error) {
+func makeProfile(root string) (string, error) {
+	u, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	home := u.HomeDir
+
+	abs, err := filepath.Abs(root)
+	if err != nil {
+		return "", err
+	}
+	root = abs
+
 	tmp := filepath.Clean(os.Getenv("TMPDIR"))
 
 	parents := append(
 		parentPaths(home, allowRead...),
 		parentPaths(home, allowWrite...)...)
 
-	relRoot, err := filepath.Rel(home, root)
-	if err != nil {
-		return "", err
+	maybeParents := []string{
+		root,
+		goEnv("GOROOT"),
+		goEnv("GOCACHE"),
+		goEnv("GOMODCACHE"),
 	}
-	if !strings.HasPrefix(relRoot, "..") {
-		parents = append(parents, parentPaths(home, relRoot)...)
+	for _, p := range maybeParents {
+		rel, err := filepath.Rel(home, p)
+		if err != nil {
+			return "", err
+		}
+		if !strings.HasPrefix(rel, "..") {
+			parents = append(parents, parentPaths(home, rel)...)
+		}
 	}
+
 	slices.Sort(parents)
 	parents = slices.Compact(parents)
+
+	wPaths := []string{
+		tmp, filepath.Join("/private", tmp),
+		root,
+		goEnv("GOCACHE"),
+		goEnv("GOMODCACHE"),
+	}
+	rPaths := []string{
+		root,
+		goEnv("GOROOT"),
+		goEnv("GOCACHE"),
+		goEnv("GOMODCACHE"),
+	}
+	for _, p := range allowRead {
+		rPaths = append(rPaths, filepath.Join(home, p))
+	}
+	for _, p := range allowWrite {
+		rPaths = append(rPaths, filepath.Join(home, p))
+		wPaths = append(wPaths, filepath.Join(home, p))
+	}
 
 	var s strings.Builder
 
@@ -64,53 +113,57 @@ func makeProfile(home, root string) (string, error) {
 	w("(allow default)")
 	w("(deny file-read* (subpath %q))", home)
 	w("(allow file-read*")
-	w("	(subpath %q)", root)
-	for _, p := range allowRead {
-		w("	(subpath %q)", filepath.Join(home, p))
+	for _, p := range rPaths {
+		w("\t(subpath %q)", p)
 	}
-	for _, p := range allowWrite {
-		w("	(subpath %q)", filepath.Join(home, p))
-	}
-	w("	(literal %q)", home)
+	w("\t; For path traversal")
+	w("\t(literal %q)", home)
 	for _, p := range parents {
-		w("	(literal %q)", p)
+		w("\t(literal %q)", p)
 	}
 	w(")")
 
 	w("(deny file-write*)")
 	w("(allow file-write*")
-	w("	(subpath %q)", root)
-	w("	(subpath %q)", tmp)
-	w("	(subpath %q)", filepath.Join("/private", tmp))
-	w("	(literal \"/dev/stdout\")")
-	w("	(literal \"/dev/stderr\")")
-	w("	(literal \"/dev/null\")")
-	w("	(literal \"/dev/ptmx\")")
-	w("	(regex #\"^/dev/ttys[0-9]*$\")")
-	for _, p := range allowWrite {
-		w("	(subpath %q)", filepath.Join(home, p))
+	for _, p := range wPaths {
+		w("\t(subpath %q)", p)
 	}
+	w("\t(literal \"/dev/null\")")
+	w("\t(literal \"/dev/ptmx\")")
+	w("\t(literal \"/dev/stderr\")")
+	w("\t(literal \"/dev/stdout\")")
+	w("\t(regex #\"^/dev/ttys[0-9]*$\")")
 	w(")")
 
 	return s.String(), nil
 }
 
-func do(dir string) error {
-	u, err := user.Current()
-	if err != nil {
-		return err
+func parentPaths(base string, in ...string) (out []string) {
+	for _, p := range in {
+		p = filepath.Dir(p)
+		for p != "." {
+			out = append(out, filepath.Join(base, p))
+			p = filepath.Dir(p)
+		}
 	}
-	profile, err := makeProfile(u.HomeDir, dir)
-	if err != nil {
-		return err
-	}
-	fmt.Println(profile)
-	return nil
+	return
 }
 
-func main() {
-	flag.Parse()
-	if err := do(flag.Arg(0)); err != nil {
-		log.Fatal(err)
+var goEnvs = make(map[string]string)
+
+func goEnv(v string) string {
+	if s, ok := goEnvs[v]; ok {
+		return s
 	}
+
+	cmd := exec.Command("go", "env", v)
+	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("go env %s failed: %v", v, err)
+	}
+	s := string(bytes.TrimSpace(out))
+
+	goEnvs[v] = s
+	return s
 }
